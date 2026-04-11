30af480313615e881a10553b177df6dbf971b53b
xayitboynormetov <xayitboynormetov55@gmail.com>
2026-04-11 16:27:09 +0500

    kep-algorith

---
+++ 16.py
@@ -0,0 +1,5 @@
+n = int(input())
+s = 1
+for son in range(1, n + 1):
+    s *= son 
+print(s)
--- 5.py
+++ 5.py
@@ -1,14 +1,24 @@
 # n = int(input())
 # s = 0
 # for son in range(1, n + 1):
-#     s += son 
+    # s += son # s = s + son 
 
 # print(s)
 
-import math
-n = int(input())
-s = 0
-for son in range(1, n + 1):
-    s += int(math.sqrt(son)) 
+# Example: n = 5
+# Sikl qadamlari:
+# 1. son = 1, 0 + 1 = 1 = s
+# 2. son = 2, 1 + 2 = 3 = s
+# 3. son = 3, 3 + 3 = 6 = s 
+# 4. son = 4, 6 + 4 = 10 = s
+# 5. son = 5, 10 + 5 = 15 = s
 
-print(s)
+
+
+# import math
+# n = int(input())
+# s = 0
+# for son in range(1, n + 1):
+#     s += int(math.sqrt(son)) 
+
+# print(s)
